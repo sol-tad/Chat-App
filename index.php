5371ee47f8b7222f7cf7559b6501b948352202d2
solabest <soltad65@gmail.com>
2024-04-30 11:13:46 -0700

    add Start up files

---
+++ index.php
@@ -0,0 +1,53 @@
+
+
+<?php include_once "header.php";
+
+?>
+<body>
+  
+<div class="wrapper">
+<section class="form signup">
+  <header>Realtime Chat app </header>
+
+  <form action="#" method="post" enctype="multipart/form-data" autocomplete="off">
+
+<div class="error-text"></div>
+<div class="name-details">
+  <div class="field input">
+   <label for="">First Name</label>
+   <input type="text" name="fname" placeholder="First name" required>
+  </div>
+  <div class="field input">
+   <label for="">Last Name</label>
+   <input type="text" name="lname" placeholder="Last name" required>
+  </div>
+
+</div>
+ <div class="field input">
+<label for=""> Email Address</label>
+   <input type="text" name="email" placeholder="Email" required>
+ </div>
+ <div class="field input">
+<label for=""> Password </label>
+   <input type="password" name="password" placeholder="Enter New Password" required>
+ </div>
+
+ <div class="field image">
+<input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg" required>
+ </div>
+ <div class="field button">
+<input type="submit" name="submit" value="Continue to Chat">
+ </div>
+  </form>
+
+
+    <div class="link">
+    Already Signed UP
+    <a href="login.php">Login now</a>
+    </div>
+</section>
+
+</div>
+
+</body>
+</html>
